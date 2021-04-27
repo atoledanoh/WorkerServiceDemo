f8340afdce54f27a351590bddbf9f142a870dd9c
Alejandro Toledano <a.toledanoh@gmail.com>
2021-04-27 14:05:37 -0300

    demo created after tthe R&D on worker service, sit-48

--- WorkerServiceDemo/Program.cs
+++ WorkerServiceDemo/Program.cs
@@ -10,36 +10,20 @@ namespace WorkerServiceDemo
     {
         public static void Main(string[] args)
         {
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-                .Enrich.FromLogContext()
-                .WriteTo.File(@"C:\logs\log.txt")
-                .CreateLogger();
-
-            try
-            {
-                Log.Information("Service has been started.");
-                CreateHostBuilder(args).Build().Run();
-                return;
-            }
-            catch (Exception e)
-            {
-                Log.Fatal(e, "Service could not be started");
-                return;
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            //adding the use of serilog
+            .UseSerilog()
                 .ConfigureServices((hostContext, services) =>
                 {
+                    //configuring the logger, taken from the Serilog Documentation page
+                    // https://github.com/serilog/serilog-settings-configuration
+                    Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(hostContext.Configuration).CreateLogger();
+                    services.AddHttpClient();
                     services.AddHostedService<Worker>();
-                })
-            .UseSerilog();
+                });
     }
 }
--- WorkerServiceDemo/Worker.cs
+++ WorkerServiceDemo/Worker.cs
@@ -1,5 +1,7 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,44 +10,108 @@ namespace WorkerServiceDemo
 {
     public class Worker : BackgroundService
     {
-        private readonly ILogger<Worker> _logger;
-        private HttpClient httpClient;
+        private readonly ILogger<Worker> logger;
+        private readonly IHttpClientFactory httpClientFactory;
+        private List<string> Urls = new List<string> {
+            "https://google.com",
+            "https://youtube.com",
+            "https://facebook.com"
+        };
 
-        public Worker(ILogger<Worker> logger)
+        /// <summary>
+        /// Using Microsoft.Extensions.Http's  Http Client Factory
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="httpClientFactory"></param>
+        public Worker(ILogger<Worker> logger, IHttpClientFactory httpClientFactory)
         {
-            _logger = logger;
+            this.logger = logger;
+            this.httpClientFactory = httpClientFactory;
         }
 
+        /// <summary>
+        /// Method override used only to log the start of the process
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         public override Task StartAsync(CancellationToken cancellationToken)
         {
-            httpClient = new HttpClient();
+            logger.LogInformation("Service has been started.");
             return base.StartAsync(cancellationToken);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="stoppingToken"></param>
+        /// <returns></returns>
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                var response = await httpClient.GetAsync("https://www.google.com/");
-
-                if (response.IsSuccessStatusCode)
+                try
                 {
-                    _logger.LogInformation("Target website is up. Status code: {StatusCode}", response.StatusCode);
+                    await PollUrls();
                 }
-                else
+                catch (Exception ex)
                 {
-                    _logger.LogError("Target Website is down. Status code: {StatusCode}", response.StatusCode);
+                    logger.LogError(ex, "An error has ocurred while polling the URLs");
                 }
-
-                await Task.Delay(5 * 1 * 1000, stoppingToken);
+                finally
+                {
+                    await Task.Delay(1 * 1 * 1000, stoppingToken);
+                }
             }
         }
 
+        /// <summary>
+        /// Method override used only to log the stopping of the process
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         public override Task StopAsync(CancellationToken cancellationToken)
         {
-            httpClient.Dispose();
-            _logger.LogInformation("Service has been stopped.");
+            logger.LogInformation("Service has been stopped.");
             return base.StopAsync(cancellationToken);
+        }
+
+        /// <summary>
+        /// Creates a list of tasks and populates them with one PollUrl task per URL in our List<string> Urls
+        /// </summary>
+        /// <returns></returns>
+        private async Task PollUrls()
+        {
+            var tasks = new List<Task>();
+            foreach (var url in Urls)
+            {
+                tasks.Add(PollUrl(url));
+            }
+            await Task.WhenAll(tasks);
+        }
+
+        /// <summary>
+        /// Task that logs whether the response status is successful or not
+        /// </summary>
+        /// <returns></returns>
+        private async Task PollUrl(string url)
+        {
+            try
+            {
+                var client = httpClientFactory.CreateClient();
+                var response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    logger.LogInformation("{Url} is online.", url);
+                }
+                else
+                {
+                    logger.LogWarning("{Url} is offline.", url);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogWarning(ex, "{Url} is offline.", url);
+            }
         }
     }
 }
